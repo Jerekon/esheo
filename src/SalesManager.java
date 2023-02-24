c09532be8f088f2c98c92f4344a7402cb60ac0e5
Jerekon <124033477+Jerekon@users.noreply.github.com>
2023-02-24 17:28:16 +0300

    Update SalesManager.java

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,12 @@
-public class SalesManager {
-    protected int[] sales;
 
-    public SalesManager(int[] sales) {
-        this.sales = sales;
-    }
+import java.util.Arrays;
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
-            if (sale > max) {
-                max = sale;
-            }
-        }
-        return max;
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new long[]{11, 21, 48, 34, 14, 1, 42,8});
+        System.out.println();
+        System.out.println("\"Analysis of sales statistics\"");
+        System.out.printf("In the specified list %s we get the maximum amount of sales : %s у.е.%n", Arrays.toString(salesManager.sales), salesManager.max());
+        System.out.println("Average calculation: " + salesManager.cropAverage() + " у.е.");
     }
 }
